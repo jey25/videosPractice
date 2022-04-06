1681304df821c4e0c9ecfe136c88849a302d50c4
jey25 <jjangeoyoung@gmail.com>
2022-04-06 19:42:09 +0900

    1
    express, server, middleware

--- src/server.js
+++ src/server.js
@@ -1,13 +1,25 @@
 import express from "express";
+import morgan from "morgan";
+import req from "express/lib/request";
 
 const PORT = 4000;
+
 const app = express();
+const logger = morgan("dev");
 
-app.get("/", () => console.log("Go home."));
+const Home = (req, res) => {
+    return res.send("I love middlewares");
+}
 
+const login = (req, res) => {
+    return res.send("login");
+}
 
+app.use(logger);
+app.get("/", Home);
+app.get("/login", login)
 
+const Listening = () => 
+console.log(`server port : ${PORT}`);
 
-const handleListening = () => console.log(`server port : ${PORT}`);
-
-app.listen(PORT, handleListening)
+app.listen(PORT, Listening)
