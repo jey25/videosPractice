243bab404cf52e517429d8d37d9cdc6db0493b58
jey25 <jjangeoyoung@gmail.com>
2022-04-18 13:23:09 +0900

    8

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,48 @@
 
-export const trending = (req, res) => res.render("home", {pageTitle : "Home"});
-export const see = (req, res) => res.render("Watch");
-export const edit = (req, res) => res.render("edit");
-export const search = (req, res) => res.send("Search Video");
-export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => res.send("Delete Video");
+    let videos = [
+        {
+            title: "first video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minute ago",
+            views: 1,
+            id:1,
+        },
+        {
+            title: "Second video",
+            rating: 4,
+            comments: 1,
+            createdAt: "5 minute ago",
+            views: 50,
+            id:2,
+        },
+        {
+            title: "3rd video",
+            rating: 8,
+            comments: 30,
+            createdAt: "12 minute ago",
+            views: 5,
+            id:3,
+        },
+    ]
+
+export const trending = (req, res) => {
+    return res.render("home", {pageTitle: "Home", videos});
+};
+export const watch = (req, res) => {
+    const { id } = req.params;
+    const video = videos[id - 1];
+    return res.render("watch", {pageTitle: `Watching: ${video.title}`, video});
+}
+export const getEdit = (req, res) => {
+    const { id } = req.params;
+    const video = videos[id - 1];
+    return res.render("edit", {pageTitle: `Editing: ${video.title}`, video});
+}
+
+export const postEdit = (req, res) => {
+    const { id } = req.params;
+    const {title} = req.body;
+    videos[id - 1].title = title;
+    return res.redirect(`/videos/${id}`);
+}
--- src/server.js
+++ src/server.js
@@ -12,6 +12,7 @@ const logger = morgan("dev");
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
+app.use(express.urlencoded({extended:true}));
 app.use("/", global);
 app.use("/videos", video);
 app.use("/users", user);
